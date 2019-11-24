7359dd4d83f9b92271b0f81d85e1e433bcaf1624
SeyfGoumeida <goumeidaahmedseyfeddine@gmail.com>
2019-11-24 23:04:21 +0100

    add method createReserve to BusController Class

--- src/com/telly/controllers/BusController.java
+++ src/com/telly/controllers/BusController.java
@@ -30,4 +30,21 @@ public class BusController {
 		return "createtrip";
 	}
 
+	@RequestMapping(value = "/createreserve", method = RequestMethod.POST)
+	public String createReserve(@Validated(FormValidationGroup.class) Bus bus, BindingResult result,
+			Principal principal) {
+
+		if (result.hasErrors()) {
+			return "reservebus";
+		}
+
+		busService.create(bus);
+
+		return "home";
+
+	}
+
+	
+
+
 }
